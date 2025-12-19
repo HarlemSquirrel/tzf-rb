764a9bc2d490bc9856b7e343d8b2789979ebda96
Kevin McCormack <kevin.mccormack@hotelengine.com>
2025-12-19 09:09:35 -0500

    Fix loading pre-compiled gems

--- lib/tzf.rb
+++ lib/tzf.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "tzf/version"
-require_relative "tzf/tzf"
+
+# Load the appropriate version of the extension
+RUBY_MINOR_VERSION = RUBY_VERSION.split('.')[0..1].join('.')
+if Dir.exist?(File.join(File.dirname(__FILE__), "tzf", RUBY_MINOR_VERSION))
+  require_relative File.join("tzf", RUBY_MINOR_VERSION, "tzf")
+else
+  require_relative File.join("tzf", "tzf")
+end
 
 ##
 # Top level module.
