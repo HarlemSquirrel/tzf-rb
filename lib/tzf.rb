761d570c6bf73fbed27c5cf8aa2e78e842167e9e
Kevin McCormack <kevin.mccormack@hotelengine.com>
2025-12-19 09:12:55 -0500

    Fix lints

--- lib/tzf.rb
+++ lib/tzf.rb
@@ -3,7 +3,7 @@
 require_relative "tzf/version"
 
 # Load the appropriate version of the extension
-RUBY_MINOR_VERSION = RUBY_VERSION.split('.')[0..1].join('.')
+RUBY_MINOR_VERSION = RUBY_VERSION.split(".")[0..1].join(".")
 if Dir.exist?(File.join(File.dirname(__FILE__), "tzf", RUBY_MINOR_VERSION))
   require_relative File.join("tzf", RUBY_MINOR_VERSION, "tzf")
 else
